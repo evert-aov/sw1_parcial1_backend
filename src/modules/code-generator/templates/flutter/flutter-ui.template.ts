561041a535d80c27e9bda81a7529f17f45577726
Evert <evert.aov@gmail.com>
2026-08-30 00:37:04 -0400

    fix(flutter-generator): import toCamelCase in flutter-ui.template.ts

--- src/modules/code-generator/templates/flutter/flutter-ui.template.ts
+++ src/modules/code-generator/templates/flutter/flutter-ui.template.ts
@@ -1,4 +1,4 @@
-import { JavaClassMeta, toSnakeCase } from '../template-models';
+import { JavaClassMeta, toSnakeCase, toCamelCase } from '../template-models';
 import { getDartFields, getPluralName } from './flutter-models';
 
 export function renderFlutterCardWidget(meta: JavaClassMeta): string {
